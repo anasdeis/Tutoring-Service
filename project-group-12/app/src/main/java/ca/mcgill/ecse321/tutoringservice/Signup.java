3d223434c70abb7db5d8b5fda6b203a3d446f423
Peiyong.Liu <charlesliu0115@gmail.com>
2019-11-27 02:13:24 -0500

    finish signup, some problems arrived

---
+++ project-group-12/app/src/main/java/ca/mcgill/ecse321/tutoringservice/Signup.java
@@ -0,0 +1,49 @@
+package ca.mcgill.ecse321.tutoringservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Signup extends AppCompatActivity implements View.OnClickListener {
+    Button homeButton;
+    Button createButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+
+        homeButton = findViewById(R.id.homeButton);
+        createButton = findViewById(R.id.createButton);
+
+        homeButton.setOnClickListener(this);
+        createButton.setOnClickListener(this);
+    }
+
+    public void openHome() {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+    }
+
+    public void openHome1() {
+        Intent intent = new Intent(this, Home1.class);
+        startActivity(intent);
+        Toast.makeText(this, "Manager Account Created!", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.homeButton:
+                openHome();
+                break;
+            case R.id.createButton:
+                openHome1();
+                break;
+        }
+    }
+}
