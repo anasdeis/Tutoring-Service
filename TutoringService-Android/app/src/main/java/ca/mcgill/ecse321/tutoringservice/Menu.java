3b658fc8080de614c95b8ed4aff1a75ce3610989
Mohammad Noor <mohammad.noor@mail.mcgill.ca>
2019-12-01 19:29:07 -0500

    logout and view tutors working!

--- TutoringService-Android/app/src/main/java/ca/mcgill/ecse321/tutoringservice/Menu.java
+++ TutoringService-Android/app/src/main/java/ca/mcgill/ecse321/tutoringservice/Menu.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse321.tutoringservice;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,8 +10,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
-public class Menu extends AppCompatActivity {
+public class Menu extends AppCompatActivity implements View.OnClickListener {
+  private Button viewTutorButton;
+  private Button logoutButton;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,34 @@ public class Menu extends AppCompatActivity {
     setContentView(R.layout.activity_menu);
 
 
+    viewTutorButton = findViewById(R.id.viewTutorButton);
+    viewTutorButton.setOnClickListener(this);
+
+    logoutButton = findViewById(R.id.logoutButton);
+    logoutButton.setOnClickListener(this);
+  }
+
+  public void openViewTutor() {
+    Intent intent = new Intent(this, viewTutors.class);
+    startActivity(intent);
+  }
+
+  public void openManagerLogin() {
+    Intent intent = new Intent(this, login.class);
+    startActivity(intent);
+  }
+
+  @Override
+  public void onClick(View v) {
+    switch (v.getId()) {
+      case R.id.viewTutorButton:
+        openViewTutor();
+        break;
+      case R.id.logoutButton:
+        openManagerLogin();
+        break;
+
+    }
   }
 
 }
---
+++ TutoringService-Android/app/src/main/java/ca/mcgill/ecse321/tutoringservice/viewTutors.java
@@ -0,0 +1,116 @@
+package ca.mcgill.ecse321.tutoringservice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
+
+public class viewTutors extends AppCompatActivity implements View.OnClickListener  {
+
+  private String error = null;
+
+  private Button homeButton;
+
+  private void refreshErrorMessage() {
+    // set the error message
+    TextView tvError = (TextView) findViewById(R.id.error);
+    tvError.setText(error);
+
+    if (error == null || error.length() == 0) {
+      tvError.setVisibility(View.GONE);
+    } else {
+      tvError.setVisibility(View.VISIBLE);
+    }
+  }
+
+  private List<String> tutorNames = new ArrayList<>();
+  private ArrayAdapter<String>  tutorAdapter;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_view_tutors);
+    Spinner managerSpinner = (Spinner) findViewById(R.id.spinner2);
+
+    tutorAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, tutorNames);
+    tutorAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    managerSpinner.setAdapter(tutorAdapter);
+
+    refreshLists(this.getCurrentFocus());
+
+    homeButton = findViewById(R.id.homeButton);
+    homeButton.setOnClickListener(this);
+  }
+  public void refreshLists(View view) {
+    refreshList(tutorAdapter ,tutorNames, "tutor/list");
+
+  }
+
+  private void refreshList(final ArrayAdapter<String> adapter, final List<String> names, final String restFunctionName) {
+    HttpUtils.get(restFunctionName, new RequestParams(), new JsonHttpResponseHandler() {
+
+      @Override
+      public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+        names.clear();
+        names.add("Please select...");
+        names.add("NAME                 TUTOR ID                 PHONE NUMBER");
+        for( int i = 0; i < response.length(); i++){
+          try {
+            names.add(response.getJSONObject(i).getString("firstName")
+              + " " + response.getJSONObject(i).getString("lastName")
+              + "               " + response.getJSONObject(i).getString("personId")
+              + "                         " + response.getJSONObject(i).getString("phoneNumber"));
+          } catch (Exception e) {
+            error += e.getMessage();
+          }
+         // refreshErrorMessage();
+        }
+        adapter.notifyDataSetChanged();
+      }
+
+      @Override
+      public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+        try {
+          error += errorResponse.get("message").toString();
+        } catch (JSONException e) {
+          error += e.getMessage();
+        }
+      //  refreshErrorMessage();
+      }
+    });
+  }
+
+
+  public void openHome() {
+    Intent intent = new Intent(this, MainActivity.class);
+    startActivity(intent);
+  }
+
+
+  @Override
+  public void onClick(View v) {
+    switch (v.getId()) {
+      case R.id.homeButton:
+        openHome();
+        break;
+
+    }
+  }
+}
